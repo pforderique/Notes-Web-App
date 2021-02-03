93bc890ed897066e31f2bef33cffd38c6ab3399d
pforderique <porderiq@mit.edu>
2021-02-03 11:25:02 -0500

    Actually, removing the extra paths and adding back the . before the module works for some reason

--- website/__init__.py
+++ website/__init__.py
@@ -1,12 +1,12 @@
 from flask import Flask
-from config import MY_SECRET_KEY
+from .config import MY_SECRET_KEY
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = MY_SECRET_KEY
 
-    from views import views
-    from auth import auth
+    from .views import views
+    from .auth import auth
 
     app.register_blueprint(views, url_prefix="/")
     app.register_blueprint(auth, url_prefix="/")
