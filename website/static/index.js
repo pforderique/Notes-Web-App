83ea0eef79467efabe896e8c87980e73b1ed378f
pforderique <porderiq@mit.edu>
2021-02-03 18:52:24 -0500

    final commit. working delete notes function

--- website/static/index.js
+++ website/static/index.js
@@ -1,1 +1,11 @@
 // personal js methods
+
+// js func for deleting notes
+function deleteNote(noteId) {
+    fetch('/delete-note', {
+        method:'POST',
+        body:JSON.stringify({ noteId: noteId })
+    }).then((_res) => {
+        window.location.href = "/"; // redirect us to homepage
+    })
+}
