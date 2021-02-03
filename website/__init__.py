4421fc20713983747cdf4aa364a0d524c3a21f3e
pforderique <porderiq@mit.edu>
2021-02-03 16:31:44 -0500

    added models

--- website/__init__.py
+++ website/__init__.py
@@ -1,9 +1,15 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from .config import MY_SECRET_KEY
+
+db = SQLAlchemy()
+DB_NAME = 'database.db'
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = MY_SECRET_KEY
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}' 
+    db.init_app(app)
 
     from .views import views
     from .auth import auth
