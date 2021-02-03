7d39dfd29762815953f1474eec13085004f6ef42
pforderique <porderiq@mit.edu>
2021-02-03 10:58:27 -0500

    added iews and auth

--- website/__init__.py
+++ website/__init__.py
@@ -1,5 +1,13 @@
 from flask import Flask
+from .config import MY_SECRET_KEY
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'k32dfsx dfs3'
+    app.config['SECRET_KEY'] = MY_SECRET_KEY
+
+    from .views import views
+    from .auth import auth
+
+    app.register_blueprint(views, url_prefix="/")
+
+    return app
